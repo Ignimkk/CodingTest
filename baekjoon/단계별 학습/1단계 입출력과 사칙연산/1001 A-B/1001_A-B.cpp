5233595d2826c58cc8e6a4374212de8ddc2ccf49
ignimkk <kmingi98@gmail.com>
2025-02-24 20:59:48 +0900

    first day complete

---
+++ baekjoon/단계별 학습/1단계 입출력과 사칙연산/1001 A-B/1001_A-B.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int A;
+int B;
+
+int main()
+{
+    std::cin >> A;
+    std::cin >> B;
+
+    std::cout << A-B << std::endl;
+
+    return 0;
+}
---
+++ baekjoon/단계별 학습/1단계 입출력과 사칙연산/1008 A divide B/1008_A_divide_B.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int A;
+int B;
+int main()
+{
+    std::cin >> A;
+    std::cin >> B;
+    std::cout << A+B << std::endl;
+    std::cout << A-B << std::endl;
+    std::cout << A*B << std::endl;
+    std::cout << A/B << std::endl;
+    std::cout << A%B << std::endl;
+
+    return 0;
+}
